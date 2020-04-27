4f1c4dbccd6a7a038b6d94e61ec8d444f47c82ba
Hanada <im@hanada.info>
2020-04-27 14:01:03 +0800

    Update static url

---
+++ static/js/config.js
@@ -0,0 +1,17 @@
+window.Config = {
+
+  SiteName: 'Tamers Union BitTracker Status',
+
+  SiteUrl: '/status',
+
+  ApiKeys: [
+    'm784383896-4e64bbd21f553201a22f7377',
+  ],
+
+  ShowLink: false,
+
+  CountDays: 60,
+
+  Navi: [
+  ]
+};
